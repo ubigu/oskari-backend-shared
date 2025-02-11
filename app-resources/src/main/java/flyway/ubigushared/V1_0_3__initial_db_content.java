8eff984065980c7df29e82c7e669c34bbb0bb0e9
Tuomas Riihimäki <tuomas.riihimaki@ubigu.fi>
2025-02-12 01:06:39 +0200

    Add other layers as non-selected

--- app-resources/src/main/java/flyway/ubigushared/V1_0_3__initial_db_content.java
+++ app-resources/src/main/java/flyway/ubigushared/V1_0_3__initial_db_content.java
@@ -3,11 +3,15 @@ package flyway.ubigushared;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.oskari.helpers.AppSetupHelper;
+import org.oskari.helpers.LayerHelper;
 
 public class V1_0_3__initial_db_content extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
         // add applications based on json under /src/main/resources/json/apps/
         AppSetupHelper.create(context.getConnection(), "/json/apps/ubigu-3067.json");
+        LayerHelper.setupLayer("/json/layers/mml-maastokartta.json");
+        LayerHelper.setupLayer("/json/layers/mml-selkokartta.json");
+        LayerHelper.setupLayer("/json/layers/mml-ortokuva.json");
     }
 }
